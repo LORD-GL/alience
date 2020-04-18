380cc569bc8063590713cded7d812988f3c8280a
LORD-GL <boikogv06@gmail.com>
2020-04-18 16:49:42 +0300

    add moving left

--- game_functions.py
+++ game_functions.py
@@ -10,10 +10,16 @@ def check_events( ship ):
                 if event.key == pygame.K_RIGHT:
                     # Начать перемещение в парво
                     ship.moving_right= True
+                elif event.key == pygame.K_LEFT:
+                    # Начать перемещение в парво
+                    ship.moving_left= True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     # Начать перемещение в парво
                     ship.moving_right= False
+                elif event.key == pygame.K_LEFT:
+                    # Начать перемещение в парво
+                    ship.moving_left = False
 
 def update_screen( game_settings, screen, ship ):
     """ Обновляет экран """
--- ship.py
+++ ship.py
@@ -16,11 +16,14 @@ class Ship():
 
         # Флаг перемещения
         self.moving_right = False
+        self.moving_left = False
 
     def update( self ):
         """ Обновляет позицию корабля с учетом флага """
         if self.moving_right:
-            self.rect.centerx += 1 
+            self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1 
 
     def blitme( self ):
         """рисует корабль в тек. позиции """
