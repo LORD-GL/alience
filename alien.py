2b01e66688f9f32c2da252fb1e2e198d97826bc9
LORD-GL <boikogv06@gmail.com>
2020-04-25 16:10:13 +0300

    add alien blit

--- alien.py
+++ alien.py
@@ -3,5 +3,21 @@ from pygame.sprite import Sprite
 
 class Alien( Sprite ):
     """ Класс, пришельца """
-    def __init__( self ):
-        pass
+    def __init__( self, game_settings, screen ):
+        super().init()
+        self.screen = screen
+        self.game_settings = game_settings
+
+        # Загрузка изображения и назначение атрибута rect
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # Каждый новый прищелец появляется в верхнем углу
+        self.rect.x = self.rect.width
+        self.rect.x = self.rect.height
+
+        self.x = float( self.rect.x )
+
+    def blitme( self ):
+        """ """
+        self.screen.blit( self.image, self.rect )
